74274ce0a43acc9c99fa694707a15745242ff460
Lillian Burnside <lillian.burnside@gmail.com>
2020-04-15 20:33:12 -0500

    added projects

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 'use strict';
+$(document).ready(function(){
+  let flag = 0;
+  $('#next').on('click', function(){
+    if(flag === 0)
+    {
+      $('.plant-app').css('transform','translate(1000%)');
+      $('.plant-app').css('display', 'none');
+      flag = 1;
+    }
+    else if(flag === 1)
+    {
+      flag = 2;
+    }
+    else if(flag === 2)
+    {
+      flag = 0;
+    }
+  });
+  $('#prev').on('click', function(){
+    if(flag === 0)
+    {
+      flag = 2;
+    }
+    else if(flag === 1)
+    {
+      $('.plant-app').css('transform', 'translate(-1000%)');
+      $('.plant-app').css('display', 'inline-block');
+      flag = 0;
+    }
+    else if(flag === 2)
+    {
+      flag = 1;
+    }
+  });    
+});
 
 function handleMenuClick() {
   $('li').on('click', function(){
